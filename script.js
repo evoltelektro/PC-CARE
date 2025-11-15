3346ebdd0165c5f17b8f8e88564a26dd320820b4
evoltelektro <evoltelektro@gmail.com>
2025-11-15 11:15:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    const menu = document.getElementById("menuPopup");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+function chatAdmin(){
+    window.open("https://wa.me/6282228266317?text=Halo%20Admin%20PC%20CARE");
+}
